c2a48d92112387fb62052bd7995781a78332ceb6
mashiike <ikeda-masashi@kayac.com>
2023-03-03 13:04:16 +0900

    refactor test

--- nowpaste_test.go
+++ nowpaste_test.go
@@ -34,82 +34,94 @@ func (f mockSlackServer) Do(r *http.Request) (*http.Response, error) {
 	return w.Result(), nil
 }
 
+type postRootTestCase struct {
+	name                  string
+	slackResponseHeaders  map[string]string
+	slackResponseBodyFile string
+	slackResponseStatus   int
+	requestHeaders        map[string]string
+	newRequestBody        func(t *testing.T) io.Reader
+}
+
+func (c postRootTestCase) Run(t *testing.T, g *goldie.Goldie, namePrefix string, middlewares ...func(func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request)) {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		dump, err := httputil.DumpRequestOut(r, true)
+		if err != nil {
+			t.Error("request dump error:", err)
+			t.FailNow()
+		}
+		g.Assert(t, namePrefix+c.name, dump)
+		fp, err := os.Open(c.slackResponseBodyFile)
+		if err != nil {
+			t.Error("can not open response data:", err)
+			t.FailNow()
+		}
+		defer fp.Close()
+		for key, value := range c.slackResponseHeaders {
+			w.Header().Set(key, value)
+		}
+		w.WriteHeader(c.slackResponseStatus)
+		io.Copy(w, fp)
+	}
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	client := newWithClient(slack.New("dummy_token", slack.OptionHTTPClient(
+		mockSlackServer(f),
+	)))
+
+	req := httptest.NewRequest(http.MethodPost, "/", c.newRequestBody(t))
+	for key, value := range c.requestHeaders {
+		req.Header.Add(key, value)
+	}
+	w := httptest.NewRecorder()
+	client.ServeHTTP(w, req)
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Error("http status not ok ", resp)
+	}
+}
+
+var postRootTestCases []postRootTestCase = []postRootTestCase{
+	{
+		name:                  "short",
+		slackResponseBodyFile: "testdata/example_chat_post_message_response.json",
+		slackResponseStatus:   http.StatusOK,
+		requestHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+		newRequestBody: func(t *testing.T) io.Reader {
+			body, _ := json.Marshal(map[string]string{
+				"channel": "#test",
+				"text":    "this is test message",
+			})
+			return bytes.NewReader(body)
+		},
+	},
+	{
+		name:                  "many_lines",
+		slackResponseBodyFile: "testdata/example_file_upload_response.json",
+		slackResponseStatus:   http.StatusOK,
+		requestHeaders: map[string]string{
+			"Content-Type": "application/json",
+		},
+		newRequestBody: func(t *testing.T) io.Reader {
+			body, _ := json.Marshal(map[string]string{
+				"channel": "#test",
+				"text":    "this is test message\nthis is test message\nthis is test message\nthis is test message\nthis is test message\nthis is test message\n",
+			})
+			return bytes.NewReader(body)
+		},
+	},
+}
+
 func TestPostRootSuccess(t *testing.T) {
 	g := goldie.New(t,
-		goldie.WithFixtureDir("testdata"),
+		goldie.WithFixtureDir("testdata/success/"),
 	)
-	cases := []struct {
-		name                  string
-		slackResponseHeaders  map[string]string
-		slackResponseBodyFile string
-		slackResponseStatus   int
-		requestHeaders        map[string]string
-		newRequestBody        func(t *testing.T) io.Reader
-	}{
-		{
-			name:                  "short",
-			slackResponseBodyFile: "testdata/example_chat_post_message_response.json",
-			slackResponseStatus:   http.StatusOK,
-			requestHeaders: map[string]string{
-				"Content-Type": "application/json",
-			},
-			newRequestBody: func(t *testing.T) io.Reader {
-				body, _ := json.Marshal(map[string]string{
-					"channel": "#test",
-					"text":    "this is test message",
-				})
-				return bytes.NewReader(body)
-			},
-		},
-		{
-			name:                  "many_lines",
-			slackResponseBodyFile: "testdata/example_file_upload_response.json",
-			slackResponseStatus:   http.StatusOK,
-			requestHeaders: map[string]string{
-				"Content-Type": "application/json",
-			},
-			newRequestBody: func(t *testing.T) io.Reader {
-				body, _ := json.Marshal(map[string]string{
-					"channel": "#test",
-					"text":    "this is test message\nthis is test message\nthis is test message\nthis is test message\nthis is test message\nthis is test message\n",
-				})
-				return bytes.NewReader(body)
-			},
-		},
-	}
-	for _, c := range cases {
-
-		client := newWithClient(slack.New("dummy_token", slack.OptionHTTPClient(
-			mockSlackServer(func(w http.ResponseWriter, r *http.Request) {
-				dump, err := httputil.DumpRequestOut(r, true)
-				if err != nil {
-					t.Error("request dump error:", err)
-					t.FailNow()
-				}
-				g.Assert(t, "post_root_success__"+c.name, dump)
-				fp, err := os.Open(c.slackResponseBodyFile)
-				if err != nil {
-					t.Error("can not open response data:", err)
-					t.FailNow()
-				}
-				defer fp.Close()
-				for key, value := range c.slackResponseHeaders {
-					w.Header().Set(key, value)
-				}
-				w.WriteHeader(c.slackResponseStatus)
-				io.Copy(w, fp)
-			}),
-		)))
-
-		req := httptest.NewRequest(http.MethodPost, "/", c.newRequestBody(t))
-		for key, value := range c.requestHeaders {
-			req.Header.Add(key, value)
-		}
-		w := httptest.NewRecorder()
-		client.ServeHTTP(w, req)
-		resp := w.Result()
-		if resp.StatusCode != http.StatusOK {
-			t.Error("http status not ok ", resp)
-		}
+	for _, c := range postRootTestCases {
+		t.Run(c.name, func(t *testing.T) {
+			c.Run(t, g, "post_root_success__")
+		})
 	}
 }
